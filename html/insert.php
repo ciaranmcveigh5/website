3c467a9f670b1c97e5f024ba3b4a2088a72d70a4
Ciaran McVeigh <cimcveigh@deloitte.co.uk>
2016-06-28 10:24:38 +0100

    [CM] check mysql connection

--- html/insert.php
+++ html/insert.php
@@ -1,5 +1,20 @@
 <?php
- 
+
+$servername = "localhost";
+$username = "cimcveigh";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
 echo $_POST['username'];
 echo '<br />';
 echo $_POST['password'];
+
+?>
