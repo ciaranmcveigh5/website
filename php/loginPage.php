8e85f40661e4a4cb6a15ac83c84a71b4c90bc326
Ciaran McVeigh <cimcveigh@deloitte.co.uk>
2016-06-30 15:41:08 +0100

    [CM] created three new pages

---
+++ php/loginPage.php
@@ -0,0 +1,128 @@
+<?php
+
+// Define variables
+$serverIP = "localhost"; // change to public ip of another vm if mysql is running else where 
+$serverUsername = "cimcveigh";
+$serverPassword = "password";
+$dbName = "website";
+$tableName = "nametable"
+$enteredUsername = $_POST['username'];
+$enteredPassword = $_POST['password'];
+$errorMessage = "";
+$required = array('username', 'password');
+
+
+
+
+//==========================================
+//	ESCAPE DANGEROUS SQL CHARACTERS
+//==========================================
+
+// Avoid SQL injection
+function quote_smart($value, $handle) {
+
+   if (get_magic_quotes_gpc()) {
+       $value = stripslashes($value); 
+   }
+
+   if (!is_numeric($value)) {
+       $value = "'" . mysql_real_escape_string($value, $handle) . "'";
+   }
+   return $value;
+}
+
+
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){ // check if the form has been posted
+	$enteredUsername = $_POST['username'];
+	$enteredPassword = $_POST['password'];
+
+	$enteredUsername = htmlspecialchars($enteredUsername); //deal with unwanted HTML (scripting attacks)
+	$enteredPassword = htmlspecialchars($enteredPassword);
+
+	//==========================================
+	//	CONNECT TO THE LOCAL DATABASE
+	//==========================================
+
+
+	$db_handle = mysql_connect($serverIP, $serverUsername, $serverPassword); // connect to database
+	$db_found = mysql_select_db($dbName, $db_handle); // check if database is found
+
+	if ($db_found) {
+
+		$enteredUsername = quote_smart($enteredUsername, $db_handle); // call function to protedt against sql injection
+		$enteredPassword = quote_smart($enteredPassword, $db_handle);
+
+		$SQL = "SELECT * FROM $dbname WHERE username = $enteredUsername AND password = $enteredPassword"; // check that username and password match one found in database, (think about using md5 function to encrypt password)
+		$result = mysql_query($SQL); // call query
+		$num_rows = mysql_num_rows($result); // how many rows where returned in case of multiple matches
+
+	//====================================================
+	//	CHECK TO SEE IF THE $result VARIABLE IS TRUE
+	//====================================================
+
+		if ($result) { // if a match is found 
+			if ($num_rows > 0) { // and more than one row is returned (why is this needed?)
+				session_start();
+				$_SESSION['login'] = "1"; // set up session variable (special type of variable), variable in this case is login with a value of one   
+				header ("Location: homePage.php");
+			}
+			else {
+				session_start();
+				$_SESSION['login'] = "";
+				header ("Location: registrationPage.php");
+			}	
+		}
+		else {
+			$errorMessage = "Error logging on";
+		}
+
+	mysql_close($db_handle);
+
+	}
+
+	else {
+		$errorMessage = "Error logging on";
+	}
+
+}
+
+?>
+
+
+<html>
+
+<head>
+
+<title>Homepage</title>
+
+<style type="text/css">
+
+</style>
+
+</head>
+
+<body>
+
+	<form action="loginPage.php" method="post" >
+	  
+		Username: <input type="text" name="username" maxlength="50" placeholder="Username" value="" />
+
+		<br />
+
+		Password: <input type="password" name="password" maxlength="50" placeholder="Password" value="" />
+
+		<br />
+
+		<input type="submit" value="Login" />
+
+	</form>
+
+	<button type="button" onclick="window.location.href='registrationPage.php'">Sign Up</button>
+
+	<P> <?PHP print $errorMessage;?> </P>
+	
+</body>
+
+</html>
---
+++ php/registrationPage.php
@@ -0,0 +1,111 @@
+<?php
+
+// Define variables
+$servername = "localhost"; // change to public ip of another vm if mysql is running else where 
+$serverUsername = "cimcveigh";
+$serverPassword = "password";
+$dbname = "website";
+$enteredUsername = $_POST['username'];
+$enteredPassword = $_POST['password'];
+$confirmedPassword = $_POST['confirmedPassword'];
+$formValid = true;
+$errorMessage = "";
+$required = array('username', 'password', 'confirmedPassword');
+
+function quote_smart($value, $handle) {
+
+   if (get_magic_quotes_gpc()) {
+       $value = stripslashes($value); 
+   }
+
+   if (!is_numeric($value)) {
+       $value = "'" . mysql_real_escape_string($value, $handle) . "'";
+   }
+   return $value;
+}
+
+// Ensure fields are not empty
+foreach($required as $field) {
+	if(empty($_POST[$field])) {
+		$formValid = false;
+		if($formValid==false) {
+			$errorMessage = "Error, empty fields";
+			break;
+		}
+	}
+}
+
+// Check Password
+if(preg_match('/[A-Z]+[a-z]+[0-9]+/', $enteredPassword)) {
+
+}else {
+	$errorMessage = "password must contain one upper case letter one lower case letter and one number"
+	$formValid = false;
+}
+
+$checkUsernameSql = mysql_query("SELECT username FROM nametable WHERE username=$enteredUsername");
+
+if($checkUsernameSql) {
+	$errorMessage = "username already in use"
+	$formValid = false;
+}
+
+// Create connection
+$conn = new mysqli($servername, $serverUsername, $serverPassword, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$insertDataSql = "INSERT INTO nametable (username, password)
+VALUES ('$enteredUsername', '$enteredPassword')";
+
+if ($formValid==true) {
+	if ($conn->query($insertDataSql) === TRUE) {
+		header ("Location: loginPage.php");
+	} else {
+    	$errorMessage = ""Error: " . $insertDataSql . "<br>" . $conn->error";
+	}
+}
+
+$conn->close();
+
+?>
+
+
+<html>
+
+<head>
+
+<title>Homepage</title>
+
+<style type="text/css">
+
+</style>
+
+</head>
+
+<body>
+
+	<form action="registrationPage.php" method="post" >
+	  
+		Username: <input type="text" name="username" maxlength="50" placeholder="Username" value="" />
+
+		<br />
+
+		Password: <input type="password" name="password" maxlength="50" placeholder="Password" value="" />
+
+		<br />
+
+		Confirm Password: <input type="password" name="confirmedPassword" maxlength="50" placeholder="Password" value="" />
+
+		<br />
+
+		<input type="submit" value="submit" />
+
+	</form>
+	
+</body>
+
+</html>
